4be1bf99eac8bf1aec8962b3f25d1c0ae1f74e4b
Ken Hare <kenhare@ultrapresence.com>
2019-05-29 10:15:36 -0600

    removed some unused modules

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { deleteDB, wrap, unwrap } from "idb";
+import { openDB } from "idb/with-async-ittr.js";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 
 import ReduxPromise from "redux-promise"; //handles promises
 import rootReducers from "./reducers";
-
+// import logger from "redux-logger";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
 const composeEnhancers =
   (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
@@ -22,6 +24,8 @@ export const store = createStore(
   rootReducers,
   composeEnhancers(applyMiddleware(ReduxPromise))
 );
+
+localStorage.removeItem("savedSearch");
 
 ReactDOM.render(
   <Provider store={store}>
