22a80f7dcc54a587e5d54d762bb1cfc50331484d
Ken Hare <kenhare@ultrapresence.com>
2019-09-20 21:30:16 -0600

    add new reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,14 @@
 import { combineReducers } from "redux";
 import MovieReducer from "./reducer_movie";
 import MapReducer from "./reducer_map";
+import LocationReducer from "./reducer_location";
+import IDBReducer from "./reducer_idb";
+
 const rootReducer = combineReducers({
   movies: MovieReducer,
-  maps: MapReducer
+  maps: MapReducer,
+  location: LocationReducer,
+  idb: IDBReducer
 });
 
 export default rootReducer;
