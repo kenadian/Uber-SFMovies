21e0a4ca690c8b228a30f7670ecc3eee005ef617
Ken Hare <kenhare@ultrapresence.com>
2019-09-20 21:34:32 -0600

    remove unused idb_reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from "redux";
 import MovieReducer from "./reducer_movie";
 import MapReducer from "./reducer_map";
 import LocationReducer from "./reducer_location";
-import IDBReducer from "./reducer_idb";
 
 const rootReducer = combineReducers({
   movies: MovieReducer,
   maps: MapReducer,
-  location: LocationReducer,
-  idb: IDBReducer
+  location: LocationReducer
 });
 
 export default rootReducer;
