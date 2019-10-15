e45f7653ba99c7ae6987b25eabc9fb1092477bb2
Ken Hare <kenhare@ultrapresence.com>
2019-10-14 21:56:32 -0600

    add functions zeroCounters, updateProgress, fixes to limit renders of details

--- src/actions/location_actions.js
+++ src/actions/location_actions.js
@@ -1,12 +1,38 @@
+import {
+  getLocationDataInBackground,
+  toggleIsGettingGooglePlaceResults,
+  mapPlaces
+} from "./map_actions";
+import store from "../store";
+
 export const LOC_DATA_REQ_MANAGER = "LOC_DATA_REQ_MANAGER";
+export const LOC_UPDATE_PROGRESS = "LOC_UPDATE_PROGRESS";
+export const LOC_ZERO_COUNTERS = "LOC_ZERO_COUNTERS";
 
-export function getLocDataReqManager(requestObject = {}) {
-  let defaultTimeout = requestObject.dataSource === "server" ? 350 : 0;
+export function zeroCounters() {
+  return { type: LOC_ZERO_COUNTERS };
+}
+
+export function updateProgress(counter) {
+  return { type: LOC_UPDATE_PROGRESS, payload: counter };
+}
+/**
+ *
+ *
+ * @export
+ * @param requestObject = {} {locationDetails:{n [n fields:'string']},request:{fields:array,query:string}}
+ * @returns Promise from
+ */
+export function getLocDataReqManager(requestObject = null) {
+  let defaultTimeout = 350;
   let overLimitTimeout = defaultTimeout * 4;
-
+  let tempCounter = 0;
   const getPlaceRecursive = (value, index, timeout, requestObjectLength) => {
     setTimeout(() => {
-      this.getLocationDataInBackground(value).then(response => {
+      store.dispatch(getLocationDataInBackground(value)).then(response => {
+        tempCounter++;
+        store.dispatch(updateProgress(tempCounter));
+
         if (
           response.payload.status &&
           response.payload.status === "OVER_QUERY_LIMIT"
@@ -23,30 +49,35 @@ export function getLocDataReqManager(requestObject = {}) {
           response.payload.status === "OK" &&
           index === requestObjectLength - 1
         ) {
-          this.toggleIsGettingGooglePlaceResults(false);
+          store.dispatch(toggleIsGettingGooglePlaceResults(false));
         }
         if (
           response.payload.status &&
           response.payload.status === "ZERO_RESULTS" &&
           index === requestObjectLength - 1
         ) {
-          this.toggleIsGettingGooglePlaceResults(false);
+          store.dispatch(toggleIsGettingGooglePlaceResults(false));
         }
         if (
           response.payload.status &&
           response.payload.status === "UNKNOWN_ERROR" &&
           index === requestObjectLength - 1
         ) {
-          this.toggleIsGettingGooglePlaceResults(false);
+          store.dispatch(toggleIsGettingGooglePlaceResults(false));
           console.error(
             `Google responded with an "${response.payload.status}" message. Try again.`
           );
         }
+        // copy googlePlaceResults1 holding state to googlePlaceResults rendering state
+        // limits the number of renders from always changing the state
+        // done when all locations have been sent and data has been received
+        if (index === requestObjectLength - 1) {
+          store.dispatch(mapPlaces());
+        }
       });
     }, timeout);
   };
-
-  requestObject.forEach((value, index) => {
+  requestObject.forEach((value, index, inputArray) => {
     getPlaceRecursive(
       value,
       index,
--- src/reducers/reducer_location.js
+++ src/reducers/reducer_location.js
@@ -1,7 +1,18 @@
-import { LOC_DATA_REQ_MANAGER } from "../actions/location_actions";
+import {
+  LOC_DATA_REQ_MANAGER,
+  LOC_UPDATE_PROGRESS,
+  LOC_ZERO_COUNTERS
+} from "../actions/location_actions";
+import { FETCH_MOVIE_BY_TITLE } from "../actions/movie_actions";
 let initialState = {};
 export default function(state = initialState, action) {
   switch (action.type) {
+    case LOC_ZERO_COUNTERS:
+      return { ...state, locationCount: null, progressCounter: 0 };
+    case FETCH_MOVIE_BY_TITLE:
+      return { ...state, locationCount: action.payload.data.length };
+    case LOC_UPDATE_PROGRESS:
+      return { ...state, progressCounter: action.payload };
     case LOC_DATA_REQ_MANAGER:
       return { ...state };
     default:
