bd56558f97accb316b271ca0f6fec08078d89e4a
Ken Hare <kenhare@ultrapresence.com>
2019-10-14 21:45:22 -0600

    change variable names

--- src/components/map.js
+++ src/components/map.js
@@ -5,16 +5,14 @@ import { connect } from "react-redux";
 class MainMap extends Component {
   componentDidMount() {
     if (!window.google) {
-      const s = document.createElement("script");
-      s.type = "text/javascript";
-      s.src = `https://maps.googleapis.com/maps/api/js?key=${
-        process.env.REACT_APP_MAPS_API_KEY
-      }&libraries=places`;
-      const x = document.getElementsByTagName("script")[0];
-      x.parentNode.insertBefore(s, x);
+      const mapScript = document.createElement("script");
+      mapScript.type = "text/javascript";
+      mapScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAPS_API_KEY}&libraries=places`;
+      const scriptElement = document.getElementsByTagName("script")[0];
+      scriptElement.parentNode.insertBefore(mapScript, scriptElement);
       // Below is important.
       //We cannot access google.maps until it's finished loading
-      s.addEventListener("load", e => {
+      mapScript.addEventListener("load", e => {
         this.props.initMap();
       });
     } else {
