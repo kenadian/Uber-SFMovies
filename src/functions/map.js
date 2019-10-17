303f13e4e65e748b9b82cbe76188bfd305aff4f3
Ken Hare <kenhare@ultrapresence.com>
2019-10-16 23:04:13 -0600

    Add inline styles, change arg sig to obj, handle width and height for image

--- src/functions/map.js
+++ src/functions/map.js
@@ -1,13 +1,24 @@
-export function makeInfoWindowContent(imgUrl, funFacts, locName) {
-  const placeImage = imgUrl ? `<img src=${imgUrl} width="250px" />` : "";
+export function makeInfoWindowContent({
+  imgUrl = null,
+  width = null,
+  locName = "No Name Supplied",
+  height = null,
+  funFacts = null
+}) {
+  // const { imgUrl, width, locName, height = null, funFacts } = windowData;
+  const placeImage = imgUrl
+    ? `<img alt=${locName} src=${imgUrl} width=${width} ${
+        imgUrl.height ? "height=" + imgUrl.height : null
+      }/>`
+    : "";
 
   const funFactsLayout = funFacts
     ? `<div className='markerFunFacts' style='font-size: 14px;font-weight:normal'>${funFacts}</div>`
     : "";
 
-  const markerWindowContent = `<div className='markerWindow' style=''>
+  const markerWindowContent = `<div className='markerWindow' style='text-align:center'>
                                     ${placeImage}
-                                    <h2 className='markerLocName'>${locName}<h2>
+                                    <h2 className='markerLocName' >${locName}<h2>
                                       ${funFactsLayout}
                                   </div>`;
 
