a114f072a4d40044359e61f5d9859e43df5197a7
Ken Hare <kenhare@ultrapresence.com>
2019-10-20 20:13:40 -0600

    Change to Component, add counter with color features

--- src/components/drawerHeader.js
+++ src/components/drawerHeader.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import {
@@ -34,14 +34,37 @@ const styles = theme => ({
   }
 });
 
-class drawerHead extends PureComponent {
-  state = {
-    anchorEl: null
-  };
+class drawerHead extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.setColor = this.setColor.bind(this);
+    this.state = {
+      anchorEl: null,
+      viewedTitleCountColour: "#3f51b5"
+    };
+  }
 
-  handleClick = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
+  setColor(viewedTitleCountColour) {
+    this.setState({ viewedTitleCountColour });
+  }
+  handleClick(event) {
+    const viewedTitlesLength = event.currentTarget.attributes.value.value;
+
+    if (viewedTitlesLength < 1) {
+      this.setColor("red");
+
+      setTimeout(() => this.setColor("#3f51b5"), 2000);
+
+      return;
+    }
+    if (viewedTitlesLength > 0) {
+      this.setState({
+        anchorEl: event.currentTarget,
+        viewedTitleCountColour: "#3f51b5"
+      });
+    }
+  }
 
   handleClose = () => {
     this.setState({ anchorEl: null });
@@ -53,7 +76,7 @@ class drawerHead extends PureComponent {
   };
 
   render() {
-    const { anchorEl } = this.state;
+    const { anchorEl, viewedTitleCountColour } = this.state;
     const {
       classes,
       theme,
@@ -70,52 +93,55 @@ class drawerHead extends PureComponent {
             <ChevronRightIcon />
           )}
         </IconButton>
-        {viewedTitles.length > 1 && (
-          <div className={classes.movieInfo}>
-            <Button
-              aria-owns={anchorEl ? "simple-menu" : undefined}
-              aria-haspopup="true"
-              onClick={this.handleClick}
-            >
-              Previously Viewed ({viewedTitles.length})
-            </Button>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={this.handleClose}
-            >
-              {viewedTitles.length > 0 &&
-                Boolean(anchorEl) &&
-                viewedTitles.map((value, index, inputArray) => {
-                  //TODO add link to load movie details.
 
-                  return (
-                    <MenuItem
-                      key={index}
-                      onClick={this.handleClose}
-                      className={classes.menuItem}
+        <div className={classes.movieInfo}>
+          <Button
+            aria-owns={anchorEl ? "simple-menu" : undefined}
+            aria-haspopup="true"
+            value={viewedTitles.length}
+            onClick={this.handleClick}
+          >
+            Previously Viewed
+            <span style={{ color: viewedTitleCountColour, marginLeft: 10 }}>
+              {`(${viewedTitles.length})`}
+            </span>
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={this.handleClose}
+          >
+            {viewedTitles.length > 0 &&
+              Boolean(anchorEl) &&
+              viewedTitles.map((value, index, inputArray) => {
+                //TODO add link to load movie details.
+
+                return (
+                  <MenuItem
+                    key={index}
+                    onClick={this.handleClose}
+                    className={classes.menuItem}
+                  >
+                    <IconButton
+                      datavalue={value.title}
+                      className={classes.deleteButton}
+                      onClick={this.handleDeletePreviousTitle}
                     >
-                      <IconButton
-                        datavalue={value.title}
-                        className={classes.deleteButton}
-                        onClick={this.handleDeletePreviousTitle}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                      <Typography
-                        key={index}
-                        datavalue={value.title}
-                        onClick={handleViewedTitlesClick}
-                      >
-                        {value.title}
-                      </Typography>
-                    </MenuItem>
-                  );
-                })}
-            </Menu>
-          </div>
-        )}
+                      <DeleteIcon />
+                    </IconButton>
+                    <Typography
+                      key={index}
+                      datavalue={value.title}
+                      onClick={handleViewedTitlesClick}
+                    >
+                      {value.title}
+                    </Typography>
+                  </MenuItem>
+                );
+              })}
+          </Menu>
+        </div>
       </div>
     );
   }
